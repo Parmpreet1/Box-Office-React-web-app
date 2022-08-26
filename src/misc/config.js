63fdadcc6f07aad70db85f5d60ddcc5ec2889780
Parmpreet1 <Parmpreet1@github.com>
2022-08-27 03:58:05 +0530

    display shows search result and use async function for feact data

---
+++ src/misc/config.js
@@ -0,0 +1,8 @@
+
+const Api_base_url="https://api.tvmaze.com"
+
+export  const GetApi =async  (queary) => {
+    const Response= await fetch(`${Api_base_url}${queary}`).then(r=>r.json())
+    return Response;
+  
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,42 @@
 import React, { useState } from 'react';
 import { MainPageLayout } from '../component/MainPageLayout';
 import { Title } from '../component/Title';
+import { GetApi } from '../misc/config';
 
 
 export const Home = () => {
   const [Input, setInput] = useState('');
+  const [Result, setResult] = useState(null);
   const onSearch=()=>{
-    const LinkPromise=fetch(`https://api.tvmaze.com/search/shows?q=${Input}`)
-    const jsonArray=LinkPromise.then((j)=>j.json())
-    jsonArray.then((Result)=>{console.log(Result)})
 
+    const jsonArray =GetApi(`/search/shows?q=${Input}`)
+    jsonArray.then((R)=>{setResult(R)})
+    console.log(Result)
   }
   const onKey=(evt)=>{
-    console.log(evt.keyCode)
     if (evt.keyCode===13) {
       onSearch()
     }
+  }
+  const RenderResult=()=>{
+    if (Result && Result.length===0) {
+      return <div><h3>No Result</h3></div>
+    }
+    if (Result && Result.length>0) {
+      return (
+        Result.map((item,index)=>{
+           return <div key={item.show.name}>{index} {item.show.name}</div>
+        })
+        )
+    }
+   
   }
   return (<>
       <Title title={"Box Office"} subtitle={"Are you looking for a movie or an actor?"}/>
       <MainPageLayout/>
       <input type="text" onChange={(e)=>setInput(e.target.value)} onKeyDown={onKey} value={Input}></input>
       <button type="button" onClick={onSearch}>Search</button>
+      {RenderResult()}
       </>
   );
 };
