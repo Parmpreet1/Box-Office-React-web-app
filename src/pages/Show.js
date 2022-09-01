56ec3ffb6a22034e0fc0e19c0ee367bfc7eb6283
Parmpreet1 <Parmpreet1@github.com>
2022-09-02 03:51:04 +0530

    added loading and error messages to show page

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,23 +1,34 @@
-import React,{useEffect,useState} from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import { GetApi } from '../misc/config';
 
-
-
 export const Show = () => {
-    const {id}=useParams();
-    const [Show, setShow] = useState(null)
+  const { id } = useParams();
+  const [Show, setShow] = useState(null);
+  const [IsLoading, setIsLoading] = useState(true);
+  const [Error, setError] = useState(null);
 
-    useEffect(() => {
-        GetApi(`/shows/${id}?embed[]=episodes&embed[]=cast&embed[]=seasons`).then(r=>setShow(r))
-      
-    
-    }, [id])
-    console.log(id,Show)
-    
-  return (
-    <div>
-        This is show page 
-    </div>
-  )
-}
+  useEffect(() => {
+    GetApi(`/shows/${id}?embed[]=episodes&embed[]=cast&embed[]=seasons`).then(
+      r => {
+        setTimeout(()=>{
+            setShow(r);
+            setIsLoading(false)
+        },2000)
+        
+      }
+    ).catch(err=>{
+        setError(err.message);
+        setIsLoading(false)
+    })
+  }, [id]);
+  console.log(id, Show);
+
+  if(IsLoading){
+    return <h2>Content is loading</h2>
+  }
+  if(Error){
+    return <h2>{Error}</h2>
+  }
+  return <h2>This is show page</h2>;
+};
